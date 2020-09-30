e3001e5a784819a774571b25b070413588e611ec
lalbuquerque <lucas.albuquerque12@gmail.com>
2020-09-30 01:08:57 -0300

    Update methods signatures to use album and artist fields

--- android/src/main/java/vn/hunghd/flutterdownloader/TaskDao.java
+++ android/src/main/java/vn/hunghd/flutterdownloader/TaskDao.java
@@ -24,7 +24,9 @@ public class TaskDao {
             TaskContract.TaskEntry.COLUMN_NAME_RESUMABLE,
             TaskContract.TaskEntry.COLUMN_NAME_OPEN_FILE_FROM_NOTIFICATION,
             TaskContract.TaskEntry.COLUMN_NAME_SHOW_NOTIFICATION,
-            TaskContract.TaskEntry.COLUMN_NAME_TIME_CREATED
+            TaskContract.TaskEntry.COLUMN_NAME_TIME_CREATED,
+            TaskContract.TaskEntry.COLUMN_NAME_ALBUM_NAME,
+            TaskContract.TaskEntry.COLUMN_NAME_ARTIST_NAME
     };
 
     public TaskDao(TaskDbHelper helper) {
@@ -32,7 +34,8 @@ public class TaskDao {
     }
 
     public void insertOrUpdateNewTask(String taskId, String url, int status, int progress, String fileName,
-                                       String savedDir, String headers, boolean showNotification, boolean openFileFromNotification) {
+                                       String savedDir, String headers, boolean showNotification, boolean openFileFromNotification,
+                                      String albumName, String artistName) {
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
         ContentValues values = new ContentValues();
@@ -48,6 +51,8 @@ public class TaskDao {
         values.put(TaskContract.TaskEntry.COLUMN_NAME_OPEN_FILE_FROM_NOTIFICATION, openFileFromNotification ? 1 : 0);
         values.put(TaskContract.TaskEntry.COLUMN_NAME_RESUMABLE, 0);
         values.put(TaskContract.TaskEntry.COLUMN_NAME_TIME_CREATED, System.currentTimeMillis());
+        values.put(TaskContract.TaskEntry.COLUMN_NAME_ALBUM_NAME, albumName);
+        values.put(TaskContract.TaskEntry.COLUMN_NAME_ARTIST_NAME, artistName);
 
         db.beginTransaction();
         try {
@@ -223,8 +228,10 @@ public class TaskDao {
         int showNotification = cursor.getShort(cursor.getColumnIndexOrThrow(TaskContract.TaskEntry.COLUMN_NAME_SHOW_NOTIFICATION));
         int clickToOpenDownloadedFile = cursor.getShort(cursor.getColumnIndexOrThrow(TaskContract.TaskEntry.COLUMN_NAME_OPEN_FILE_FROM_NOTIFICATION));
         long timeCreated = cursor.getLong(cursor.getColumnIndexOrThrow(TaskContract.TaskEntry.COLUMN_NAME_TIME_CREATED));
+        String albumName = cursor.getString(cursor.getColumnIndexOrThrow(TaskContract.TaskEntry.COLUMN_NAME_ALBUM_NAME));
+        String artistName = cursor.getString(cursor.getColumnIndexOrThrow(TaskContract.TaskEntry.COLUMN_NAME_ARTIST_NAME));
         return new DownloadTask(primaryId, taskId, status, progress, url, filename, savedDir, headers,
-                mimeType, resumable == 1, showNotification == 1, clickToOpenDownloadedFile == 1, timeCreated);
+                mimeType, resumable == 1, showNotification == 1, clickToOpenDownloadedFile == 1, timeCreated, albumName, artistName);
     }
 
 }
