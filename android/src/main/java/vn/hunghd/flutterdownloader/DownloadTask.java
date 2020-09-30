bda7f973946d3a64d6d47eb211dd2c17a9b85f7a
lalbuquerque <lucas.albuquerque12@gmail.com>
2020-09-30 01:11:25 -0300

    Add albumName and artistName to DownloadTask

--- android/src/main/java/vn/hunghd/flutterdownloader/DownloadTask.java
+++ android/src/main/java/vn/hunghd/flutterdownloader/DownloadTask.java
@@ -14,9 +14,12 @@ public class DownloadTask {
     boolean showNotification;
     boolean openFileFromNotification;
     long timeCreated;
+    String albumName;
+    String artistName;
 
     DownloadTask(int primaryId, String taskId, int status, int progress, String url, String filename, String savedDir,
-                 String headers, String mimeType, boolean resumable, boolean showNotification, boolean openFileFromNotification, long timeCreated) {
+                 String headers, String mimeType, boolean resumable, boolean showNotification, boolean openFileFromNotification,
+                 long timeCreated, String albumName, String artistName) {
         this.primaryId = primaryId;
         this.taskId = taskId;
         this.status = status;
@@ -30,10 +33,12 @@ public class DownloadTask {
         this.showNotification = showNotification;
         this.openFileFromNotification = openFileFromNotification;
         this.timeCreated = timeCreated;
+        this.albumName = albumName;
+        this.artistName = artistName;
     }
 
     @Override
     public String toString() {
-        return "DownloadTask{taskId=" + taskId + ",status=" + status + ",progress=" + progress + ",url=" + url + ",filename=" + filename + ",savedDir=" + savedDir + ",headers=" + headers + "}";
+        return "DownloadTask{taskId=" + taskId + ",status=" + status + ",progress=" + progress + ",url=" + url + ",filename=" + filename + ",savedDir=" + savedDir + ",headers=" + headers + ",albumName=" + albumName + ",artistName=" + artistName + "}";
     }
 }
